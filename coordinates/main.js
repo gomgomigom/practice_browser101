c7ac5488d6bc90aea85c3e4636bc3441984dee8e
gomgomigom <nodam89@naver.com>
2021-10-27 14:19:29 +0900

    JS: performance improvement

--- coordinates/main.js
+++ coordinates/main.js
@@ -3,18 +3,22 @@
 const vertical = document.querySelector('.vertical');
 const horozontal = document.querySelector('.horozontal');
 const target = document.querySelector('.target');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 const tag = document.querySelector('.tag');
+console.log(targetRect);
 
-document.addEventListener('mousemove', e => {
-  const x = e.clientX;
-  const y = e.clientY;
-  console.log(x, y);
-  vertical.style.top = `${y}px`;
-  horozontal.style.left = `${x}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
+addEventListener('load', () => {
+  document.addEventListener('mousemove', e => {
+    const x = e.clientX;
+    const y = e.clientY;
+    console.log(x, y);
+    vertical.style.transform = `translateY(${y}px)`;
+    horozontal.style.transform = `translateX(${x}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px,${y - targetHalfHeight}px)`;
+    tag.style.transform = `translate(${x + 20}px,${y + 20}px)`;
+  });
 });
 
 document.addEventListener('mousedown', () => {
