f4e261ae64fc2775c38678791303073baca3be84
gomgomigom <nodam89@naver.com>
2021-11-03 14:19:29 +0900

    Carrot-JS: start button toggle

--- carrot/main.js
+++ carrot/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const start = document.querySelector('.start');
+
+// start button
+start.addEventListener('click', e => {
+  console.log(e.target);
+  if (e.target.tagName !== 'I') {
+    return;
+  }
+  if (e.target.className == 'fas fa-play') {
+    e.target.className = 'fas fa-square';
+  } else {
+    e.target.className = 'fas fa-play';
+  }
+});
