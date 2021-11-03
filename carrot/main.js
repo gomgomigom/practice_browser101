4c938e19b027d00962524e901f574aac01459263
gomgomigom <nodam89@naver.com>
2021-11-03 15:52:13 +0900

    Carrot JS: random bug

--- carrot/main.js
+++ carrot/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const start = document.querySelector('.start');
+const playground = document.querySelector('.playground');
 
 // start button
 start.addEventListener('click', e => {
@@ -14,3 +15,37 @@ start.addEventListener('click', e => {
     e.target.className = 'fas fa-play';
   }
 });
+
+// count down
+let count = 9;
+function timer() {
+  if (count >= 0) {
+    document.querySelector('.timer').innerHTML = count;
+    count--;
+  } else {
+    return;
+  }
+}
+setInterval(timer, 1000);
+
+// random
+const playgroundY = playground.getBoundingClientRect().height;
+const playgroundX = playground.getBoundingClientRect().width;
+const randomX = playgroundX * Math.random() * 0.9;
+const randomY = playgroundY * Math.random() * 0.9;
+console.log(playgroundY);
+console.log(playgroundX);
+
+console.log(randomX);
+console.log(randomY);
+
+function addBugAndCarrot() {
+  playground.innerHTML = `
+  <img style="top: ${randomY}px; left:${randomX}px;" src="./img/bug.png" alt="벌레" class="bug" />
+  `;
+}
+{
+  /* <img style="top: ${randomY}px; left:${randomX}px;" src="./img/carrot.png" alt="당근" class="carrot" /> */
+}
+
+addBugAndCarrot();
