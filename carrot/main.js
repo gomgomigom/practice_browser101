9c498b828b0fdcc1ffe466e80b5053e81f0f1ffb
gomgomigom <nodam89@naver.com>
2021-11-04 10:39:21 +0900

    Carrot JS: random carrot pull

--- carrot/main.js
+++ carrot/main.js
@@ -2,6 +2,9 @@
 
 const start = document.querySelector('.start');
 const playground = document.querySelector('.playground');
+const items = document.querySelector('.items');
+const count = document.querySelector('.count');
+const alertDiv = document.querySelector('.alert');
 
 // start button
 start.addEventListener('click', e => {
@@ -17,11 +20,13 @@ start.addEventListener('click', e => {
 });
 
 // count down
-let count = 9;
+let coundDown = 9;
 function timer() {
-  if (count >= 0) {
-    document.querySelector('.timer').innerHTML = count;
-    count--;
+  if (coundDown >= 0) {
+    document.querySelector('.timer').innerHTML = `
+    0:${coundDown}
+    `;
+    coundDown--;
   } else {
     return;
   }
@@ -31,29 +36,46 @@ setInterval(timer, 1000);
 // random
 const playgroundY = playground.getBoundingClientRect().height;
 const playgroundX = playground.getBoundingClientRect().width;
-const randomX = playgroundX * getRandom() * 0.9;
-const randomY = playgroundY * getRandom() * 0.7;
-console.log(playgroundY);
-console.log(playgroundX);
 
-console.log(randomX);
-console.log(randomY);
-function getRandom() {
-  return Math.random();
+let id = 0;
+function repeat(itemsName) {
+  for (let i = 0; i < 10; i++) {
+    let randomX = playgroundX * Math.random() * 0.9;
+    let randomY = playgroundY * Math.random() * 0.7;
+    const obj = document.createElement('img');
+    obj.setAttribute('style', `top: ${randomY}px; left:${randomX}px;`);
+    obj.setAttribute('src', `./img/${itemsName}.png`);
+    obj.setAttribute('alt', `${itemsName}`);
+    obj.setAttribute('data-id', id);
+    obj.setAttribute('class', `${itemsName}`);
+    items.appendChild(obj);
+    id++;
+  }
 }
+repeat('carrot');
+repeat('bug');
 
-function addBugAndCarrot() {
-  playground.innerHTML = `
-  <img style="top: ${randomY}px; left:${randomX}px;" src="./img/carrot.png" alt="당근" class="carrot" /> 
-  `;
-}
+// carrot pull
+const carrots = document.querySelectorAll('.carrot');
+carrots.forEach(element => {
+  console.log(element.dataset.id);
+  element.addEventListener('click', e => {
+    const carrotId = e.target.dataset.id;
+    const toBePull = document.querySelector(`.carrot[data-id="${carrotId}"`);
+    toBePull.remove();
+    console.log(carrotId);
+  });
+});
 
-{
-  /* <img style="top: ${randomY}px; left:${randomX}px;" src="./img/bug.png" alt="벌레" class="bug" /> */
-}
+// count carrot
+document.addEventListener('click', () => {
+  const carrots = document.querySelectorAll('.carrot');
+  const countNumber = carrots.length;
+  console.log(countNumber);
+  count.innerHTML = countNumber;
+  if (countNumber == '0') {
+    alertDiv.classList.add('active');
+  }
+});
 
-addBugAndCarrot();
-console.log(randomX);
-console.log(randomY);
-console.log(randomX);
-console.log(randomY);
+// alert
